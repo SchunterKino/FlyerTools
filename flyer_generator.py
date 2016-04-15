ddfbc90fa93f5f53a92028bbd283995417e94d7a
Mike Klimek <m.klimek@tu-bs.de>
2016-04-15 17:26:50 +0200

    Fix invalid paths when program file is in different directory

--- flyer_generator.py
+++ flyer_generator.py
@@ -25,7 +25,8 @@ locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
 
 class FlyerTransformation(object):
 
-    def __init__(self, program, movie, next_movies):
+    def __init__(self, resources_dir, program, movie, next_movies):
+        self.resources_dir = resources_dir
         self.program = program
         self.movie = movie
         self.next_movies = next_movies
@@ -45,14 +46,14 @@ class FlyerTransformation(object):
 
     def apply(self, template):
         self.parse(template)
+        movie_dir = os.path.join(self.resources_dir, self.movie['Dateien']['Ordner'])
 
         # title
         title = self.movie['Titel']
         self.getElementById('titel').text = title
 
         # text
-        folder = self.movie['Dateien']['Ordner']
-        textfile_path = os.path.join(folder, self.movie['Dateien']['Text'])
+        textfile_path = os.path.join(movie_dir, self.movie['Dateien']['Text'])
         with open(textfile_path) as text_file:
             text = text_file.read().decode('utf-8')
         self.getElementById('kurztext').text = text
@@ -87,15 +88,15 @@ class FlyerTransformation(object):
         self.getElementById('eintrittFilmpass').text = pass_price
 
         # poster
-        poster = os.path.join(folder, self.movie['Dateien']['Poster'])
+        poster = os.path.join(movie_dir, self.movie['Dateien']['Poster'])
         self.getElementById('poster').set('{http://www.w3.org/1999/xlink}href', encode_image(poster))
 
         # screenshots
-        screenshot1 = os.path.join(folder, self.movie['Dateien']['Screenshot1'])
+        screenshot1 = os.path.join(movie_dir, self.movie['Dateien']['Screenshot1'])
         self.getElementById('screenshot1').set('{http://www.w3.org/1999/xlink}href', encode_image(screenshot1))
-        screenshot2 = os.path.join(folder, self.movie['Dateien']['Screenshot2'])
+        screenshot2 = os.path.join(movie_dir, self.movie['Dateien']['Screenshot2'])
         self.getElementById('screenshot2').set('{http://www.w3.org/1999/xlink}href', encode_image(screenshot2))
-        screenshot3 = os.path.join(folder, self.movie['Dateien']['Screenshot3'])
+        screenshot3 = os.path.join(movie_dir, self.movie['Dateien']['Screenshot3'])
         self.getElementById('screenshot3').set('{http://www.w3.org/1999/xlink}href', encode_image(screenshot3))
 
         # posters and dates for next movies
@@ -119,7 +120,9 @@ class FlyerTransformation(object):
                     next_dates.append(short_date)
                 next_dates = ' + '.join(next_dates)
                 next_title = next_movie['Titel']
-                next_poster = os.path.join(next_movie['Dateien']['Ordner'], next_movie['Dateien']['Poster'])
+                next_poster = os.path.join(self.resources_dir,
+                                           next_movie['Dateien']['Ordner'],
+                                           next_movie['Dateien']['Poster'])
                 next_poster_element.set('{http://www.w3.org/1999/xlink}href', encode_image(next_poster))
             self.getElementById('folgeDatum' + str(element_id)).text = next_dates
             self.getElementById('folgeTitel' + str(element_id)).text = next_title
@@ -157,28 +160,28 @@ def compress(image_path):
     return temp_file.getvalue()
 
 
-def title_to_filepath(titel, output_folder):
+def title_to_filepath(titel, output_dir):
     # filter special chars
     filename = re.sub('[^\w\-_\. ]', '_', titel)
-    return os.path.join(output_folder, filename + '.svg')
+    return os.path.join(output_dir, filename + '.svg')
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    # TODO Print version?
     # TODO Generate (compressed) PDFs?
-    # TODO Program Flyer
-    # parser.add_argument('-p', '--program', help='Programm Flyer erstellen', action='store_true')
-    parser.add_argument('-f', '--force', help='Bestehende Flyer überschreiben', action='store_true')
+    # TODO Overview flyer
+    parser.add_argument('-f', '--force', help=u'Bestehende Flyer überschreiben', action='store_true')
     parser.add_argument('-o', '--output', help='Ausgabe Ordner', action='store', default='woechentliche Flyer')
-    parser.add_argument('Datei', type=argparse.FileType('r'), help='JSON Datei mit dem aktuellen Programm')
+    parser.add_argument('-t', '--template', help='Vorlage Datei', action='store', default='vorlage.svg')
+    parser.add_argument('Program', type=argparse.FileType('r'), help='JSON Datei mit dem aktuellen Programm')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
-    program = json.load(args.Datei)['Programm']
-    template = program['Vorlage']
+    template = args.template
+    resources_dir = os.path.dirname(args.Program.name)  # resources are relative to the program file
+    program = json.load(args.Program)['Programm']
 
     # generate flyers
     movies = program['Filme']
@@ -190,18 +193,18 @@ if __name__ == '__main__':
             print 'Flyer existiert bereits:\t' + filepath
             continue
 
-        # create folder if not exists
-        if not os.path.exists(args.output):
-            os.makedirs(args.output)
-
         # collect next 3 movies (or None) for preview
         next_movies = movies[index+1:index+4]
         next_movies = next_movies + [None]*(3 - len(next_movies))
 
         # generate
-        print 'Generiere Flyer:\t' + filepath
-        transformation = FlyerTransformation(program, movie, next_movies)
+        print 'Generiere Flyer:\t' + os.path.basename(filepath)
+        transformation = FlyerTransformation(resources_dir, program, movie, next_movies)
         document = transformation.apply(template)
+
+        # create output directory if not exists
+        if not os.path.exists(args.output):
+            os.makedirs(args.output)
 
         # save to file
         with open(filepath, 'w') as output_file:
